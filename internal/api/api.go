c045474f25273a6203e496f4ed986f9992bdc6aa
Michael Recachinas <mrecachinas@spectric.com>
2021-02-18 14:33:29 -0500

    Move websocket handler method under websocket.go

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
+
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/streadway/amqp"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"net/http"
-	"time"
 )
 
 // GetStatus returns a single status object
@@ -127,64 +127,3 @@ func (a *Api) StopTask(c echo.Context) error {
 var (
 	upgrader = websocket.Upgrader{}
 )
-
-// UpdaterWebsocket handles broadcasting updates to the database;
-// Note: this just means querying the collection every
-// `api.Cfg.PollingInterval` seconds and returning the *entire*
-// contents of the collection.
-func (a *Api) UpdaterWebsocket(c echo.Context) error {
-	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		c.Logger().Error(err)
-		return err
-	}
-	defer ws.Close()
-
-	// Create a new connection entry in our connection map
-	a.Websocket.Lock()
-	a.Websocket.Connections[ws] = struct{}{}
-	defer a.Websocket.CloseWebsocketConnection(ws)
-	a.Websocket.Unlock()
-
-	msg := fmt.Sprintf(
-		"Client %s joined. %d total connections.",
-		c.Request().Host,
-		len(a.Websocket.Connections),
-	)
-	c.Logger().Info(msg)
-
-	delay := time.Duration(a.Cfg.PollingInterval) * time.Second
-
-	// TODO: Make error handling/logging more useful? Break if error occurs?
-	// TODO: Figure out a way to break gracefully on ctrl-c?
-	for {
-		select {
-		case <-time.After(delay):
-			// Get ALL records every N seconds
-			statusList, err := GetAllStatus(a.DB.Database(a.Cfg.MongoDatabaseName))
-			if err != nil {
-				c.Logger().Error("Error getting all status from database")
-			}
-
-			// Broadcast result to all connections
-			err = a.Websocket.SendMessageToPool(statusList)
-			if err != nil {
-				c.Logger().Error("Error sending message to pool")
-			}
-
-			// Read inbound websocket messages;
-			// if we get an error back, that means the client
-			// closed the connection
-			_, _, err = ws.ReadMessage()
-			if err != nil {
-				msg := fmt.Sprintf(
-					"Client %s quitting. %d connections remain.",
-					c.Request().Host,
-					len(a.Websocket.Connections),
-				)
-				c.Logger().Info(msg)
-				break
-			}
-		}
-	}
-}
--- internal/api/websocket.go
+++ internal/api/websocket.go
@@ -1,8 +1,73 @@
 package api
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
 )
+
+// UpdaterWebsocket handles broadcasting updates to the database;
+// Note: this just means querying the collection every
+// `api.Cfg.PollingInterval` seconds and returning the *entire*
+// contents of the collection.
+func (a *Api) UpdaterWebsocket(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		c.Logger().Error(err)
+		return err
+	}
+	defer ws.Close()
+
+	// Create a new connection entry in our connection map
+	a.Websocket.Lock()
+	a.Websocket.Connections[ws] = struct{}{}
+	defer a.Websocket.CloseWebsocketConnection(ws)
+	a.Websocket.Unlock()
+
+	msg := fmt.Sprintf(
+		"Client %s joined. %d total connections.",
+		c.Request().Host,
+		len(a.Websocket.Connections),
+	)
+	c.Logger().Info(msg)
+
+	delay := time.Duration(a.Cfg.PollingInterval) * time.Second
+
+	// TODO: Make error handling/logging more useful? Break if error occurs?
+	// TODO: Figure out a way to break gracefully on ctrl-c?
+	for {
+		select {
+		case <-time.After(delay):
+			// Get ALL records every N seconds
+			statusList, err := GetAllStatus(a.DB.Database(a.Cfg.MongoDatabaseName))
+			if err != nil {
+				c.Logger().Error("Error getting all status from database")
+			}
+
+			// Broadcast result to all connections
+			err = a.Websocket.SendMessageToPool(statusList)
+			if err != nil {
+				c.Logger().Error("Error sending message to pool")
+			}
+
+			// Read inbound websocket messages;
+			// if we get an error back, that means the client
+			// closed the connection
+			_, _, err = ws.ReadMessage()
+			if err != nil {
+				msg := fmt.Sprintf(
+					"Client %s quitting. %d connections remain.",
+					c.Request().Host,
+					len(a.Websocket.Connections),
+				)
+				c.Logger().Info(msg)
+				break
+			}
+		}
+	}
+}
 
 func (pool *WebsocketConnectionPool) SendMessageToPool(message interface{}) error {
 	pool.RLock()
